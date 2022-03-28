c2b40d541aff3f3612aa951793b55df50e0befd1
Verda05 <verda1988@gmail.com>
2022-03-28 14:08:39 +0300

    Амирханов Б.А.

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,24 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
+    private Animal animal;
 
     @Autowired
-    private Animal animal;
+    public AnimalsCage(@Qualifier("dog")Animal animal) {
+        this.animal = animal;
+    }
+
+    @Autowired
+    private Timer timer;
+
+    public Timer getTimer() {
+        return timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
